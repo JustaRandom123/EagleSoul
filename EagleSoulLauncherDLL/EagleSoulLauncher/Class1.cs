5bb568d216319a4b883c31dd6df5e623337d71f9
JustaRandom123 <47950815+JustaRandom123@users.noreply.github.com>
2020-11-03 21:26:21 +0100

    EagleSoulLauncherDLL

---
+++ EagleSoulLauncherDLL/EagleSoulLauncher/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+
+namespace EagleSoulLauncher
+{
+    public class DbConnector
+    {
+
+        public static WebClient wclient = new WebClient();
+        public static string login(string username,string password)
+        {
+            string checkLogin = wclient.DownloadString("http://66.70.154.61/handler.php?action=login&username=" + username + "&password=" + password + "&key=mkfcrYIXnZQDRMXh");
+            return checkLogin;
+        }
+
+        public static string register(string username, string password)
+        {
+            string timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+            string checkRegister = wclient.DownloadString("http://66.70.154.61/handler.php?action=register&username=" + username + "&password=" + password + "&timestamp=" + timestamp + "&key=mkfcrYIXnZQDRMXh");
+            return checkRegister;
+        }
+    }
+}
